d0acf38a6bd97110919e46f7203994a0926ce61f
krishnadey30 <krishnakumardey.dey@gmail.com>
2019-04-04 04:34:01 +0530

    Logo add

--- README.md
+++ README.md
@@ -1,46 +1,53 @@
-# Jupyter Kernel Chapel
+=====================
+Jupyter Kernel Chapel
+=====================
 
-## Using Chapel in Jupyter
+Using Chapel in Jupyter
+**************************
+
 
 It is possible to use [Jupyter](http://www.jupyter.org) as front-end for Chapel.
 
-### Installation
+Installation
+############
 
 These installation instructions are for Ubuntu Linux 16.04 (Xenial).
 
-#### Python and Jupyter
+Python and Jupyter
+~~~~~~~~~~~~~~~~~~
 
 If you want to run the Jupyter notebook locally on your computer, you need a recent version
 of Python 2 or 3 and Jupyter installed. The use of Python 3 is recommended.
 
-To install Python3 run
+**To install Python3 run**
 
-```bash
+.. code-block:: bash
 
-apt-get install python3-pip
-```
+  apt-get install python3-pip
+
 
 We recommend using Python3, but you can also use Python2.
 
-To install Jupyter using pip, run
+**To install Jupyter using pip, run**
 
-```bash
+.. code-block:: bash
 
-pip3 install jupyter
-```
+  pip3 install jupyter
 
-#### Chapel
+
+Chapel
+~~~~~~
 
 Please install Chapel in shared mode
 
-```bash
+.. code-block:: bash
 
-export CHPL_LIB_PIC=shared
-```
+  export CHPL_LIB_PIC=shared
 
-#### Jupyter Kernel Chapel
 
-```bash
-pip3 install jupyter-kernel-chapel
+Jupyter Kernel Chapel
+~~~~~~~~~~~~~~~~~~~~~
 
-```
+.. code-block:: bash
+
+  pip3 install jupyter-kernel-chapel
--- setup.py
+++ setup.py
@@ -3,45 +3,11 @@ import json
 import os
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from glob import glob
 
-
-class Installer(install):
-     def run(self):
-          # Regular install
-          install.run(self)
-
-          # Post install
-          print('Installing Ansible Kernel kernelspec')
-          from jupyter_client.kernelspec import KernelSpecManager
-          from IPython.utils.tempdir import TemporaryDirectory
-          kernel_json = {
-          "argv": [
-               "python3",
-               "-m",
-               "jupyter-kernel-chapel",
-               "-f",
-               "{connection_file}"
-          ],
-          "display_name": "Chapel",
-          "language": "chapel",
-          "codemirror_mode": "chapel"
-          }
-          
-          with TemporaryDirectory() as td:
-               os.chmod(td, 0o755)  # Starts off as 700, not user readable
-               with open(os.path.join(td, 'kernel.json'), 'w') as f:
-                    json.dump(kernel_json, f, sort_keys=True)
-               # TODO: Copy resources once they're specified
-
-               print('Installing IPython kernel spec')
-               KernelSpecManager().install_kernel_spec(td, 'chapel', user=self.user, replace=True, prefix=self.prefix)
-
-
-
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
-
+kernelpath = os.path.join("share", "jupyter", "kernels", "chapel")
+nbextpath = os.path.join("share", "jupyter", "nbextensions", "chapel-mode")
+nbconfpath = os.path.join("etc", "jupyter", "nbconfig", "notebook.d")
 
 setup( name="jupyter-kernel-chapel"
      , version="0.0.4"
@@ -52,7 +18,10 @@ setup( name="jupyter-kernel-chapel"
      , url="https://github.com/krishnadey30/jupyter_kernel_chapel"
      , download_url='https://github.com/krishnadey30/jupyter_kernel_chapel/tarball/0.0.1'
      , packages=find_packages()
-     , cmdclass={'install': Installer}
      , keywords=['jupyter', 'notebook', 'kernel', 'chapel']
      , include_package_data=True
+     , data_files=[(kernelpath, glob("jupyter-kernel-chapel/resources/*")),
+                   (kernelpath, glob("jupyter-kernel-chapel/kernel.json")),
+                   (nbextpath, glob("jupyter-kernel-chapel/chapel-mode/*")),
+                   (nbconfpath, glob("jupyter-kernel-chapel/chapel-mode.json"))]
      )
