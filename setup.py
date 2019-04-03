dfd85b2b5edc17cf68f9889e7bbf29da50a8b08e
krishnadey30 <krishnakumardey.dey@gmail.com>
2019-04-04 05:04:00 +0530

    added dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import json
 import os
+import codecs
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 from glob import glob
@@ -9,13 +10,16 @@ kernelpath = os.path.join("share", "jupyter", "kernels", "chapel")
 nbextpath = os.path.join("share", "jupyter", "nbextensions", "chapel-mode")
 nbconfpath = os.path.join("etc", "jupyter", "nbconfig", "notebook.d")
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+def read(fname):
+    return codecs.open(fpath(fname), encoding='utf-8').read()
 
 setup( name="jupyter-kernel-chapel"
      , version="0.0.4"
      , description="A Jupyter kernel for chapel"
-     , long_description=long_description
+     , long_description=read(fpath('README.rst'))
      , author="Krishna Kumar Dey"
      , author_email="krishnakumardey.dey@gmail.com"
      , url="https://github.com/krishnadey30/jupyter_kernel_chapel"
@@ -23,6 +27,10 @@ setup( name="jupyter-kernel-chapel"
      , packages=find_packages()
      , keywords=['jupyter', 'notebook', 'kernel', 'chapel']
      , include_package_data=True
+     , install_requires=[
+        'jupyter-core',
+        'jupyter_client',
+        ]
      , data_files=[(kernelpath, glob("jupyter-kernel-chapel/resources/*")),
                    (kernelpath, glob("jupyter-kernel-chapel/kernel.json")),
                    (nbextpath, glob("jupyter-kernel-chapel/chapel-mode/*")),
